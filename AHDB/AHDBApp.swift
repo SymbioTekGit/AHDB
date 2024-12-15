365bb308b56a800fda2a2bdfd3ee5aa4cead84b0
Symbiosis Technology <symbiotek2012@gmail.com>
2024-12-15 15:47:36 +0400

    Initial Commit

---
+++ AHDB/AHDBApp.swift
@@ -0,0 +1,32 @@
+//
+//  AHDBApp.swift
+//  AHDB
+//
+//  Created by Alvin Heib on 15/12/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AHDBApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
